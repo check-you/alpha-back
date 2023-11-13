1db6d842f78b5ff4c9aa22d0a1c14436ee8daf23
yoj9168@gmail.com <yoj9168@gmail.com>
2023-11-13 20:01:39 +0900

    first commit

---
+++ src/main/java/com/kids/alpha/shinhansec/ShinhansecApplication.java
@@ -0,0 +1,13 @@
+package com.kids.alpha.shinhansec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinhansecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinhansecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kids/alpha/shinhansec/ShinhansecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kids.alpha.shinhansec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinhansecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
