b45fc83d59c1b91dce98cf18348975eb4ae5a5ad
yoj9168@gmail.com <yoj9168@gmail.com>
2023-11-20 18:12:40 +0900

    chg: 요청 url 변화

--- src/main/java/com/checkyou/shinhansec/config/SecurityConfig.java
+++ src/main/java/com/checkyou/shinhansec/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import com.checkyou.shinhansec.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,6 +14,14 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -32,34 +42,35 @@ public class SecurityConfig {
                 .antMatchers("/api/account/*").hasRole("USER")
                 .antMatchers("/api/user/*").permitAll()
                 .antMatchers("/api/main/*").permitAll()
+                .antMatchers("/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
-//    @Component
-//    @Order(Ordered.HIGHEST_PRECEDENCE)
-//    public class CorsFilter extends OncePerRequestFilter {
-//        @Override
-//        protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-//            System.out.println("cors called");
-//            response.setHeader("Access-Control-Allow-Origin", "");
-//            response.setHeader("Access-Control-Allow-Credentials", "true");
-//            response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
-//            response.setHeader("Access-Control-Max-Age", "3600");
-//            response.setHeader("Access-Control-Allow-Headers",
-//                    "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-xsrf-token");
-//            response.setHeader("Access-Control-Expose-Headers", "Accesstoken");
-//
-//            // allow cros preflight
-//            if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-//                response.setStatus(HttpServletResponse.SC_OK);
-//            } else {
-//                filterChain.doFilter(request, response);
-//            }
-//        }
-//    }
+    @Component
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public static class CorsFilter extends OncePerRequestFilter {
+        @Override
+        protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+            System.out.println("cors called");
+            response.setHeader("Access-Control-Allow-Origin", "");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers",
+                    "Origin, X-Requested-With, Content-Type, Accept, Authorization, x-xsrf-token");
+            response.setHeader("Access-Control-Expose-Headers", "Accesstoken");
+
+            // allow cros preflight
+            if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+                response.setStatus(HttpServletResponse.SC_OK);
+            } else {
+                filterChain.doFilter(request, response);
+            }
+        }
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
